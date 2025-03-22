1866b4a2e6a0a312ac4f842ce9851d7c04111720
Luke Turnbull <luke.turnbull@fabra.com>
2025-03-22 20:23:19 +1100

    Flip clip space y coordinate so 0,0 is the top left like most 2d graphics apis

--- pages/web-gl/fundamentals/classes/main.js
+++ pages/web-gl/fundamentals/classes/main.js
@@ -22,7 +22,7 @@ const vertexShaderSource = /* glsl */ `#version 300 es
     // convert from 0->2 to -1->+1 (clip space)
     vec2 clipSpace = zeroToTwo - 1.0;
 
-    gl_Position = vec4(clipSpace, 0, 1);
+    gl_Position = vec4(clipSpace * vec2(1, -1), 0, 1);
   }
 `;
 
